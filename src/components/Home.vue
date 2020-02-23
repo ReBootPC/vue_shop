3d9038a51f2636bace2acdae5487877ca687b8c9
ReBootPC <2773666587@qq.com>
2020-02-23 18:54:34 +0800

    4th init project

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,121 @@
 <template>
-    <div>
-        <el-button type="info" @click="logout">退出</el-button>
-    </div>
+    <el-container class="home-container">
+        <!--页面头部区域-->
+        <el-header>
+            <div>
+                <img src="../assets/heima.png">
+                <span>电商后台管理系统</span>
+            </div>
+            <el-button type="info" @click="logout">退出</el-button>
+        </el-header>
+        <!--页面主体区域-->
+        <el-container>
+            <!--侧边栏-->
+            <el-aside :width="isCollapse ? '64px' : '200px'">
+                <div class="toggle-button" @click="toggleCollapse">|||</div>
+                <el-menu background-color="#333744" text-color="#fff" active-text-color="#409eff" unique-opened :collapse="isCollapse" :collapse-transition="false" router :default-active="activePath">
+                    <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id">
+                        <template slot="title">
+                            <i :class="iconObj[item.id]"></i>
+                            <span>{{item.authName}}</span>
+                        </template>
+                        <el-menu-item :index="'/' + subItem.path" v-for="subItem in item.children" :key="subItem.id" @click="saveNavState('/' + subItem.path)">
+                            <template slot="title">
+                                <i class="el-icon-menu"></i>
+                                <span>{{subItem.authName}}</span>
+                            </template>
+                        </el-menu-item>
+                    </el-submenu>
+                </el-menu>
+            </el-aside>
+            <!--右侧主体区域-->
+            <el-main>
+                <router-view></router-view>
+            </el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
     export default {
         name: "Home",
+        data() {
+          return {
+              menulist: [],
+              iconObj: {
+                  '125': 'el-icon-s-custom',
+                  '103': 'el-icon-s-tools',
+                  '101': 'el-icon-s-goods',
+                  '102': 'el-icon-s-order',
+                  '145': 'el-icon-s-data'
+              },
+              isCollapse: false,
+              activePath: ''
+          }
+        },
+        created() {
+            this.getMenuList()
+            this.activePath = window.sessionStorage.getItem('activePath')
+        },
         methods: {
             logout() {
                 window.sessionStorage.clear();
                 this.$router.push('/login');
+            },
+            async getMenuList() {
+               const { data: res } = await this.$http.get('menus')
+                if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+                this.menulist = res.data
+                console.log(res)
+            },
+            toggleCollapse() {
+                //点击按钮，侧边栏的折叠或展开
+                this.isCollapse = !this.isCollapse
+            },
+            saveNavState(activePath) {
+                window.sessionStorage.setItem('activePath', activePath)
+                this.activePath = activePath
             }
         }
     }
 </script>
 
 <style lang="less" scoped>
-
+    .home-container {
+        height: 100%;
+    }
+    .el-header {
+        background-color: #373d41;
+        display: flex;
+        justify-content: space-between;
+        padding-left: 0;
+        align-items: center;
+        color: #fff;
+        font-size: 20px;
+        > div {
+            display: flex;
+            align-items: center;
+            span {
+                margin-left: 15px;
+            }
+        }
+    }
+    .el-aside {
+        background-color: #333744;
+        .el-menu {
+            border-right: none;
+        }
+    }
+    .el-container {
+        background-color: #eaedf1;
+    }
+    .toggle-button {
+        background-color: #4a5064;
+        font-size: 10px;
+        line-height: 24px;
+        color: #fff;
+        text-align: center;
+        letter-spacing: 0.2em;
+        cursor: pointer;
+    }
 </style>
